8809af5d657d7305fb338d3693bb6d7dfa9127e0
prolingua <prolingua.geo@gmail.com>
2022-11-12 19:03:54 +0700

    Update GoldToken.sol

--- contracts/GoldToken.sol
+++ contracts/GoldToken.sol
@@ -2,9 +2,11 @@
 
 pragma solidity 0.8.17;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "hardhat/console.sol";
 
 contract GoldToken is ERC20 {
     constructor(uint256 initialSupply) ERC20("Gold", "GLD") {
+        console.log("deployer: ", msg.sender);
         _mint(msg.sender, initialSupply);
     }
 }
